a3a0b03b6aded691c6298f491b3ca561ff0f12d3
anri kitazawa <anrimoku21@gmail.com>
2026-02-02 15:24:35 +0900

    DiaryDetailView と SettingUpdateView を追加

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class SettingUpdateView(UpdateView):
+    model = User
+    fields = ['is_advice']
+    template_name = 'setting_update.html'
+
+    def get_object(self):
+        return self.request.user
